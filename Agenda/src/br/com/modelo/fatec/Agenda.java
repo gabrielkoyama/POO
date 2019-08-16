79a2e9c7d5bc2395c0337b990976ee6c64ec4106
gabrielkoyamao <gabriel.koyama.alves@hotmail.com>
2019-08-16 10:33:10 -0300

    POO 16/08 10:30

--- Agenda/src/br/com/modelo/fatec/Agenda.java
+++ Agenda/src/br/com/modelo/fatec/Agenda.java
@@ -5,10 +5,24 @@ import br.com.negocio.fatec.Controle;
 public class Agenda {
 	public Contato[] contatos = new Contato[5];
 	
+	public void listarContato() {
+		for (int i = 0; i < contatos.length; i++) {
+			if(contatos[i] != null) {
+				System.out.println("Nome: " + contatos[i].nome);				
+			}
+		}
+	}
+	
 	public void inserirContato() {
 		System.out.println("Por favor, digite um nome para o contato");
 		Contato c = new Contato();
 		Controle ctrl = new Controle();
 		c.nome = ctrl.texto();
+		for (int i = 0; i < contatos.length; i++) {
+			if(contatos[i] == null) {
+				contatos[i] = c;
+				break;
+			}
+		}
 	}
 }
--- Agenda/src/br/com/sistema/fatec/App.java
+++ Agenda/src/br/com/sistema/fatec/App.java
@@ -1,5 +1,6 @@
 package br.com.sistema.fatec;
 
+import br.com.modelo.fatec.Agenda;
 import br.com.negocio.fatec.Controle;
 import br.com.negocio.fatec.Menu;
 
@@ -7,12 +8,19 @@ public class App {
 	public static void main(String[] args) {
 		int opc = 100;
 		Menu m = new Menu();
+		Agenda agenda = new Agenda();
 		m.boasVindas();
 		
 		while(opc != 0) {
 			m.menu();
 			Controle ctrl = new Controle();
 			opc = ctrl.opcao();
+			if(opc == 1) {
+				agenda.inserirContato();
+			}
+			if(opc == 2) {
+				agenda.listarContato();
+			}
 		}	
 	}
 }
